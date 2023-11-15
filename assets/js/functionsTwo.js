196a43c07fcdc483440722a43c0e72bdae9aafe1
maylova93 <142877128+maylova93@users.noreply.github.com>
2023-11-15 23:47:37 +0100

    namedFunction

--- assets/js/functionsTwo.js
+++ assets/js/functionsTwo.js
@@ -4,3 +4,9 @@
 helloWorld("hello world, med parameter");
 
 // her skal du skrive din funktion
+function helloWorld(n1) {
+    console.log("hello world, med parameter");
+}
+function n1() {
+    return 1;
+}
